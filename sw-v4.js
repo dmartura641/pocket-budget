a0cf4f7ede42a9f6f00de0faac72540a8960d950
dmartura641 <dmartura@depaul.edu>
2026-08-28 13:13:12 -0500

    Add files via upload

---
+++ sw-v4.js
@@ -0,0 +1,42 @@
+const CACHE='pocket-budget-v4';
+const ASSETS=['./v4.html','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(event.request).then(r=>r||caches.match('./v4.html')))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        if(event.request.method==='GET' && response.status===200){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(event.request))
+  );
+});
